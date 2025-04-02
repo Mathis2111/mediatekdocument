a906bb4a496c24cea14e11a496bffb658846010d
Mathis2111 <148047075+Mathis2111@users.noreply.github.com>
2025-04-02 15:59:24 +0200

    MediatekDocuments-ModelTests + Solution

---
+++ MediatekDocuments-ModelTests/CommandeTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MediaTekDocuments.model;
+
+namespace MediatekDocuments_ModelTests
+{
+    /// <summary>
+    /// Classe de test unitaire pour la classe métier Commande
+    /// </summary>
+    [TestClass]
+    public class CommandeTests
+    {
+        private const string id = "00060";
+        private static readonly DateTime dateCommande = new DateTime(2025, 2, 22);
+        private const double montant = 50;
+
+        private static readonly Commande commande = new Commande(id, dateCommande, montant);
+
+        /// <summary>
+        /// Teste le constructeur de la classe Commande
+        /// </summary>
+        [TestMethod()]
+        public void CommandeTest()
+        {
+            Assert.AreEqual(id, commande.Id, "devrait réussir : id valorisé");
+            Assert.AreEqual(dateCommande, commande.DateCommande, "devrait réussir : date de commande valorisée");
+            Assert.AreEqual(montant, commande.Montant, "devrait réussir : montant valorisé");
+        }
+    }
+
+    
+}
---
+++ MediatekDocuments-ModelTests/ServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MediaTekDocuments.model;
+
+namespace MediatekDocuments_ModelTests
+{
+    /// <summary>
+    /// Classe de test unitaire pour la classe métier Service
+    /// </summary>
+    [TestClass]
+    public class ServiceTests
+    {
+        private const string libelle = "Administrateur";
+
+        private static readonly Service service = new Service(libelle);
+
+        /// <summary>
+        ///  Teste le constructeur de la classe Service
+        /// </summary>
+        [TestMethod()]
+        public void ServiceTest()
+        {
+            Assert.AreEqual(libelle, service.Libelle, "devrait réussir : libellé valorisé");
+        }
+    }
+}
